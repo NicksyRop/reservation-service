76127c102ce065969d65567024c68b31c71030e0
Nickson Kipkorir <nicksonkipkorir25@gmail.com>
2023-08-31 16:36:35 +0300

    added logic for rest communication to rental service

--- src/main/java/org/car/rental/reservation/rest/ReservationResource.java
+++ src/main/java/org/car/rental/reservation/rest/ReservationResource.java
@@ -6,10 +6,13 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import org.car.rental.RentalClient;
 import org.car.rental.reservation.Car;
 import org.car.rental.reservation.InMemoryReservationsRepository;
 import org.car.rental.reservation.InventoryClient;
 import org.car.rental.reservation.Reservation;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestQuery;
 
 import java.time.LocalDate;
@@ -26,9 +29,12 @@ public class ReservationResource {
 
     private final InMemoryReservationsRepository inMemoryReservationsRepository;
 
-    public ReservationResource(InventoryClient inventoryClient, InMemoryReservationsRepository inMemoryReservationsRepository) {
+    private final RentalClient rentalClient;
+
+    public ReservationResource(InventoryClient inventoryClient, InMemoryReservationsRepository inMemoryReservationsRepository ,@RestClient RentalClient rentalClient) {
         this.inventoryClient = inventoryClient;
         this.inMemoryReservationsRepository = inMemoryReservationsRepository;
+        this.rentalClient = rentalClient;
     }
 
     @GET
@@ -52,7 +58,12 @@ public class ReservationResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @POST
     public  Reservation save(Reservation reservation){
-        return inMemoryReservationsRepository.save(reservation);
+        Reservation result = inMemoryReservationsRepository.save(reservation);
+        String userId = "x";
+        if(reservation.startDate.equals(LocalDate.now())){
+            rentalClient.start(userId, result.id);
+        }
+        return result;
     }
 
 
